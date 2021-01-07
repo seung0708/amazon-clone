c87d9ab3e804d1bd989ebf34e9713c06a1f73506
seung_kim <seunglives@hotmail.com>
2021-01-07 15:36:22 -0800

    fixed Subtotal

--- src/components/SubTotal.js
+++ src/components/SubTotal.js
@@ -10,25 +10,31 @@ function SubTotal() {
     const [{ basket }, dispatch] = useStateValue();
   
     return (
+       
       <div className="subtotal">
         <CurrencyFormat
-          renderText={(value) => (
-            <>
-              <p>
-                Subtotal ({basket.length} items): <strong>{value}</strong>
-              </p>
-              <small className="subtotal-gift">
-                <input type="checkbox" /> This order contains a gift
-              </small>
-            </>
-          )}
-          decimalScale={2}
-          value={getBasketTotal(basket)}
-          displayType={"text"}
-          thousandSeparator={true}
-          prefix={"$"}
+        
+        renderText={(value) => (
+          <>
+            <p>
+          
+              Subtotal ({basket.length} items): <strong>{value}</strong>
+            </p>
+           
+            <small className="subtotal-gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        
+        decimalScale={2}
+        value={getBasketTotal(basket)}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+     
         />
-  
+        
         <button onClick={e => history.push('/payment')}>Proceed to Checkout</button>
       </div>
     );
